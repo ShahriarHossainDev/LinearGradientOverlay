4230007e24b8a8f7c278be6710c8dab5cad5ade9
ShahriarHossainDev <shahriar.hossain.dev@gmail.com>
2024-09-27 12:31:27 +0600

    Initial Commit

---
+++ LinearGradientOverlay/LinearGradientOverlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinearGradientOverlayApp.swift
+//  LinearGradientOverlay
+//
+//  Created by Shishir_Mac on 27/9/24.
+//
+
+import SwiftUI
+
+@main
+struct LinearGradientOverlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
